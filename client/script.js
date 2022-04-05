c2feae4262ace3622f36df312dc34386470ba27e
Prishal <92685854+Prishal-git@users.noreply.github.com>
2022-04-05 17:52:12 +0100

    Added collapsing functionality to individual post

--- client/script.js
+++ client/script.js
@@ -1,1 +1,15 @@
+function toggleComments() {
+  document.getElementById("commentsSection").classList.toggle("hide");
+}
 
+function togglePostBody() {
+  document.getElementById("postBodySection").classList.toggle("min");
+
+  let btn = document.querySelector(".readMoreBtn").innerHTML;
+
+  if (btn === "Read More") {
+    btn.innerHTML = "Read Less";
+  } else {
+    btn.innerHTML;
+  }
+}
