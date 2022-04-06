a864f508cd241fbbc4381656c87ca75b1d652f09
Prishal <92685854+Prishal-git@users.noreply.github.com>
2022-04-06 16:13:45 +0100

    Emoji counter logic added
    Data not storing though yet

--- client/script.js
+++ client/script.js
@@ -2,26 +2,25 @@
 //Event listener for CREATE POST//
 //************************************************************************************************/
 const form = document.querySelector("form");
-const API_URL = "http://localhost:3000/journal"
+const API_URL = "http://localhost:3000/journal";
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const formData = new FormData(form);
   const title = formData.get("postTitleContent");
   const Content = formData.get("postBodyContent");
- 
+
   post = {
     title,
-    Content
-};
+    Content,
+  };
   fetch(API_URL, {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify(post),
     headers: {
-      'content-type': 'application/json'
-    }
-
-  })
+      "content-type": "application/json",
+    },
+  });
 });
 
 //************************************************************************************************//
@@ -40,6 +39,73 @@ function togglePostBody() {
   } else if (btn === "Read less") {
     document.querySelector(".readMoreBtn").innerText = "Read more";
   }
+}
+
+//************************************************************************************************//
+// Emoji counter logic//
+//************************************************************************************************//
+
+function emojiCounter1() {
+  let x;
+  let emojiCounter = parseInt(
+    document.querySelector(".vEmojiCounterText1").innerText
+  );
+  if (emojiCounter > 0) {
+    x = emojiCounter;
+  } else {
+    x = 0;
+    document
+      .getElementById("vEmojiBtn1")
+      .classList.toggle("btn-outline-primary");
+    document
+      .getElementById("hEmojiBtn1")
+      .classList.toggle("btn-outline-primary");
+  }
+  x = x + 1;
+  document.querySelector(".vEmojiCounterText1").innerText = x;
+  document.querySelector(".hEmojiCounterText1").innerText = x;
+}
+
+function emojiCounter2() {
+  let x;
+  let emojiCounter = parseInt(
+    document.querySelector(".vEmojiCounterText2").innerText
+  );
+  if (emojiCounter > 0) {
+    x = emojiCounter;
+  } else {
+    x = 0;
+    document
+      .getElementById("vEmojiBtn2")
+      .classList.toggle("btn-outline-primary");
+    document
+      .getElementById("hEmojiBtn2")
+      .classList.toggle("btn-outline-primary");
+  }
+  x = x + 1;
+  document.querySelector(".vEmojiCounterText2").innerText = x;
+  document.querySelector(".hEmojiCounterText2").innerText = x;
+}
+
+function emojiCounter3() {
+  let x;
+  let emojiCounter = parseInt(
+    document.querySelector(".vEmojiCounterText3").innerText
+  );
+  if (emojiCounter > 0) {
+    x = emojiCounter;
+  } else {
+    x = 0;
+    document
+      .getElementById("vEmojiBtn3")
+      .classList.toggle("btn-outline-primary");
+    document
+      .getElementById("hEmojiBtn3")
+      .classList.toggle("btn-outline-primary");
+  }
+  x = x + 1;
+  document.querySelector(".vEmojiCounterText3").innerText = x;
+  document.querySelector(".hEmojiCounterText3").innerText = x;
 }
 
 /*const data = {postTitle, posttext};
